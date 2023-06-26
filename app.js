ca13fc5867a12317944a901618707fe5addcc1d1
Dillon Ellis <dillonzellis@gmail.com>
2023-06-26 10:30:21 -0400

    added instructions to not copy reset in styles

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const primary = document.querySelector(".primary-text");
 const secondary = document.querySelector(".secondary-text");
 
 const creditUnion = "1st Choice";
-const day = "Monday";
-const date = "May 31st";
-const holiday = "Memorial Day";
-const openDay = "Tuesday, June 1st";
+const day = "Tuesday";
+const date = "July 4th";
+const holiday = "Independence Day";
+const openDay = "Wednesday, July 5th";
 
 const primaryText = `We will be closed on ${day}, ${date} in observance of ${holiday}.`;
 const secondaryText = `${openDay} we will be open with regular business hours.`;
@@ -16,7 +16,7 @@ secondary.textContent = secondaryText;
 const notifBar = document.querySelector(".notification-bar");
 const currentDate = new Date();
 const startDate = new Date("June 10, 2023 17:00:00");
-const endDate = new Date("June 19, 2023 23:59:59");
+const endDate = new Date("July 4, 2023 23:59:59");
 
 const toggleElementByDate = (startDate, EndDate) => {
   if (currentDate >= startDate && currentDate <= endDate) {
