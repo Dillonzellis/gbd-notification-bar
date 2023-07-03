4b5adef30699a27c81c2cac8269d4b89f2dafb26
dillonzellis <dillonzellis@gmail.com>
2023-07-03 15:21:10 -0400

    overhaul to handle multiple notification bars now

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
-const primary = document.querySelector(".primary-text");
-const secondary = document.querySelector(".secondary-text");
+const notifications = [
+  {
+    primaryText: 'We will be closed on Tuesday, July 4th in observance of Independence Day.',
+    secondaryText: 'Wednesday, July 5th we will be open with regular business hours.',
+    startDate: new Date("June 10, 2023 17:00:00"),
+    endDate: new Date("July 4, 2023 23:59:59"),
+    fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
+    brandingPrimary: '#1a4789',
+    brandingAccent: '#c70000'
+  },
+  {
+    primaryText: 'The Midtown Branch will be closed on Tuesday, July 25th',
+    secondaryText: 'We apologize in advance for any inconvenience.',
+    startDate: new Date("July 5, 2023 00:00:00"),
+    endDate: new Date("July 25, 2023 23:59:59"),
+    fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
+    brandingPrimary: '#1B5D6C',
+    brandingAccent: '#37B04C'
+  },
+];
 
-const creditUnion = "1st Choice";
-const day = "Tuesday";
-const date = "July 4th";
-const holiday = "Independence Day";
-const openDay = "Wednesday, July 5th";
-
-const primaryText = `We will be closed on ${day}, ${date} in observance of ${holiday}.`;
-const secondaryText = `${openDay} we will be open with regular business hours.`;
-
-primary.textContent = primaryText;
-secondary.textContent = secondaryText;
-
-const notifBar = document.querySelector(".notification-bar");
 const currentDate = new Date();
-const startDate = new Date("June 10, 2023 17:00:00");
-const endDate = new Date("July 4, 2023 23:59:59");
 
-const toggleElementByDate = (startDate, EndDate) => {
-  if (currentDate >= startDate && currentDate <= endDate) {
-    notifBar.classList.toggle("hidden");
+function createNotificationBar(notification) {
+  const notificationBar = document.createElement("div");
+  notificationBar.classList.add("notification-bar", "hidden");
+
+  const container = document.createElement("div");
+  container.classList.add("container-de", "text-content");
+
+  const primary = document.createElement("div");
+  primary.classList.add("primary-text");
+  primary.textContent = notification.primaryText;
+
+  container.appendChild(primary);
+
+  if (notification.secondaryText) {
+    const secondary = document.createElement("div");
+    secondary.classList.add("secondary-text");
+    secondary.textContent = notification.secondaryText;
+
+    container.appendChild(secondary);
   }
-};
 
-toggleElementByDate(startDate, endDate);
+  notificationBar.style.setProperty("--font-family", notification.fontFamily);
+  notificationBar.style.setProperty("--brandingPrimary", notification.brandingPrimary);
+  notificationBar.style.setProperty("--brandingAccent", notification.brandingAccent);
+
+  notificationBar.appendChild(container);
+
+  if (currentDate >= notification.startDate && currentDate <= notification.endDate) {
+    notificationBar.classList.toggle("hidden");
+  }
+
+  return notificationBar;
+}
+
+const container = document.querySelector("#notification-container");
+
+notifications.forEach(notification => {
+  const notificationBar = createNotificationBar(notification);
+  container.appendChild(notificationBar);
+});
