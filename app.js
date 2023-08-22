348b5ffcee7be448d35a39735d9acbbb0dc98574
dillonzellis <dillonzellis@gmail.com>
2023-08-22 14:39:51 -0400

    add new excel notification bar

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const notifications = [
   {
-    primaryText: 'We will be closed on Tuesday, July 4th in observance of Independence Day.',
-    secondaryText: 'Wednesday, July 5th we will be open with regular business hours.',
+    primaryText: 'Our Midtown Branch will be closed on Tuesday, August 25.',
+    secondaryText: 'We apologize for any inconvenience.',
     startDate: new Date("June 10, 2023 17:00:00"),
-    endDate: new Date("July 4, 2023 23:59:59"),
+    endDate: new Date("August 25, 2023 23:59:59"),
     fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
-    brandingPrimary: '#1a4789',
+    brandingPrimary: '#164752',
     brandingAccent: '#c70000'
   },
   {
-    // primaryText: '',
-    secondaryText: 'Excel FCU will never ask you for your online banking information, pin number(s), or password(s).  Beware of fraudsters posing as Excel FCU via text, email or phone. Do not hesitate to call the credit union directly if you have questionable requests for information.',
+    primaryText: 'We will be closed in observance of Labor Day on Monday, September 4.',
+    // secondaryText: 'Excel FCU will never ask you for your online banking information, pin number(s), or password(s).  Beware of fraudsters posing as Excel FCU via text, email or phone. Do not hesitate to call the credit union directly if you have questionable requests for information.',
     startDate: new Date("July 5, 2023 00:00:00"),
-    endDate: new Date("July 25, 2024 23:59:59"),
+    endDate: new Date("September 4, 2023 23:59:59"),
     fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
     brandingPrimary: '#D20000',
     brandingAccent: '#1B5D6C'
