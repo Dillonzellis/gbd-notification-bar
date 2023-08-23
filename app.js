113fd1a8ac50e1fc80fe209ff77810366c42ba51
dillonzellis <dillonzellis@gmail.com>
2023-08-23 14:00:18 -0400

    excel add back fraud notif bar

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const notifications = [
   {
+    // primaryText: '',
+    secondaryText: 'Excel FCU will never ask you for your online banking information, pin number(s), or password(s).  Beware of fraudsters posing as Excel FCU via text, email or phone. Do not hesitate to call the credit union directly if you have questionable requests for information.',
+    startDate: new Date("July 5, 2023 00:00:00"),
+    endDate: new Date("July 25, 2024 23:59:59"),
+    fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
+    brandingPrimary: '#D20000',
+    brandingAccent: '#1B5D6C'
+  },
+  {
     primaryText: 'Our Midtown Branch will be closed on Tuesday, August 29.',
     secondaryText: 'We apologize for any inconvenience.',
     startDate: new Date("June 10, 2023 17:00:00"),
@@ -10,11 +19,10 @@ const notifications = [
   },
   {
     primaryText: 'We will be closed in observance of Labor Day on Monday, September 4.',
-    // secondaryText: 'Excel FCU will never ask you for your online banking information, pin number(s), or password(s).  Beware of fraudsters posing as Excel FCU via text, email or phone. Do not hesitate to call the credit union directly if you have questionable requests for information.',
     startDate: new Date("July 5, 2023 00:00:00"),
     endDate: new Date("September 4, 2023 23:59:59"),
     fontFamily: '"Lexend Deca", Helvetica, Arial, Lucida, sans-serif',
-    brandingPrimary: '#D20000',
+    brandingPrimary: '#0A3161',
     brandingAccent: '#1B5D6C'
   },
 ];
